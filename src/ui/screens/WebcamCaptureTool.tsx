53955aef4cd9ae9a9290ad4951f9863c9b85dff0
Michelle Dick <michelled@kiva.org>
2020-11-30 19:12:33 -0800

    PRO-2546 Add image uploader to issuer tool

--- src/ui/screens/WebcamCaptureTool.tsx
+++ src/ui/screens/WebcamCaptureTool.tsx
@@ -4,7 +4,7 @@ import {flowController} from "../KernelContainer";
 import Camera from 'react-html5-camera-photo';
 import Button from '@material-ui/core/Button';
 import 'react-html5-camera-photo/build/css/index.css';
-
+import ImageUpload from './ImageUpload';
 export interface Props {
   setCredentialCreationData(credentialCreationData: any): void,
   credentialCreationData: any
@@ -35,6 +35,13 @@ export default class WebcamCaptureTool extends React.Component<any,any> {
     })
   }
 
+  handleUploadPhoto = (dataUri: string) => {
+    const photo = dataUri.replace("data:image/png;base64,","")
+    this.setState({
+      "photo~attach": photo
+    });
+  }
+
   handleTakePhoto = (dataUri: string) => {
     const photo = dataUri.replace("data:image/png;base64,","")
     this.setState({
@@ -42,20 +49,61 @@ export default class WebcamCaptureTool extends React.Component<any,any> {
     });
   }
 
-  render() {
+  renderPageButtons() {
     return (
-      <Grid>
-        <Camera
-          onTakePhoto = { (dataUri : any) => { this.handleTakePhoto(dataUri); } }
-        />
-        <WebcamCaptureToolButtons
-          onClickBack={() => flowController.goTo('BACK')}
-          onSubmit={() => this.saveCredentialCreationData()}
-          onReset={() => this.onReset()}
-        ></WebcamCaptureToolButtons>
-      </Grid>
-      
-    );
+      <WebcamCaptureToolButtons
+        onClickBack={() => flowController.goTo('BACK')}
+        onSubmit={() => this.saveCredentialCreationData()}
+        onReset={() => this.onReset()}
+      ></WebcamCaptureToolButtons>
+    )
+  }
+
+  render() {
+    if (this.state["photo~attach"]) {
+      return (
+        <Grid
+          container
+          justify="space-around"
+          xs={12}>
+          <img src={"data:image/png;base64," + this.state["photo~attach"]}></img>
+          { this.renderPageButtons() }
+        </Grid>
+      )
+    } else {
+      return (
+        <Grid container justify="space-around">
+          <Grid
+            item
+            xs={8}>
+            <Grid container justify="space-around">
+              <Grid
+                item
+                direction="row"
+                xs={6}
+                md={5}>
+                <ImageUpload
+                  handleUploadPhoto={this.handleUploadPhoto}></ImageUpload>
+              </Grid>
+              <Grid
+                item
+                xs={6}
+                md={5}
+                >
+                <div className="camera-container">
+                  Or take a photo
+                  <Camera
+                    isFullscreen = { false }
+                    onTakePhoto = { (dataUri : any) => { this.handleTakePhoto(dataUri); } }
+                  />
+                </div>
+              </Grid>
+              { this.renderPageButtons() }
+            </Grid>
+          </Grid>
+        </Grid>
+      );
+    }
   }
 };
 
