12ef94965601c1abfa0d4cb9f574841c4adb0cdc
Michelle <michelled@kiva.org>
2020-12-01 05:09:57 -0800

    PRO-2546 Add image uploader to issuer tool (#7)
    * PRO-2546 Add image uploader to issuer tool
    
    * PRO-2547 Phone number input screen for group b

---
+++ src/ui/screens/ImageUpload.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import "../css/ImageUpload.css";
+import Grid from '@material-ui/core/Grid';
+
+export interface Props {
+    handleUploadPhoto: Function
+}
+
+export default class ImageUpload extends React.Component<Props> {
+ 
+    constructor(props: Props) {
+      super(props);
+      this.onChangeHandler = this.onChangeHandler.bind(this);
+    }
+
+    convertBase64(file: File) {
+        return new Promise((resolve, reject) => {
+          const fileReader = new FileReader();
+          fileReader.readAsDataURL(file)
+          fileReader.onload = () => {
+            resolve(fileReader.result);
+          }
+          fileReader.onerror = (error) => {
+            reject(error);
+          }
+        })
+      }
+ 
+    async onChangeHandler(event: any) {
+        const file = event.target.files[0]
+        const base64 = await this.convertBase64(file)
+        this.props.handleUploadPhoto(base64);
+    }
+ 
+    render() {
+        return (
+            <form method="post" action="#" id="#">
+                Upload Your File
+                <div className="form-group files">
+                    <input type="file" className="form-control" onChange={this.onChangeHandler} />
+                </div>
+            </form>
+        );
+    }
+}
